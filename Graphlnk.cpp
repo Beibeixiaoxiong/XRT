3fe25cb0511e72904935886db055c6ea5ec0e943
xiongruoting <34154858+xiongruoting@users.noreply.github.com>
2017-12-25 09:29:25 +0800

    Update Graphlnk.cpp

--- Graphlnk.cpp
+++ Graphlnk.cpp
@@ -43,6 +43,175 @@ Graphlnk::Graphlnk(int sz)
  	numEdges=0;
  	NodeTable=new Vertex[sz];
  	for(int i=0;i<maxVertices;i++)
- 	NodeTable[i].adj=NULL;
+ 	Node int main()
+{
+Graphlnk G(30);
+cout<<"读取文件插入站点" <<endl;
+G.insertVertex();
+cout<<"总站点数为"<<G.numVertices<<endl;
+int i,j,weight;
+ifstream fin("C:\\2.txt", std::ios::in);
+char line[1024]={0};
+string x = "";
+string y="";
+int z;
+while(fin.getline(line, sizeof(line)))
+{
+stringstream word(line);
+word >> x;
+word >> y;
+word >> z;
+i=G.getVertexPos(x);
+j=G.getVertexPos(y);
+weight=z;
+G.insertEdge(i,j,weight);
+}
+
+fin.clear();
+fin.close();
+cout<<"总边数数为"<<G.numEdges<<endl;
+ 
+	//查询函数
+	int *dist= new int[G.numVertices];
+	 int *path= new int[G.numEdges];
+	 string v,u;
+	 cout<<"输入路由器"<<endl;
+	 cin>>v;
+	 int h;
+	 int r=G.getVertexPos(v);
+	 if(r==-1)
+	 {
+	 	cout<<"没有该路由"<<endl;
+	 }
+	 else
+	 {	 
+	 ShortestPath(G, r, dist, path);
+     }
+	cout<<"删除路由器"<<endl;
+	cin>>v;
+	int w=G.getVertexPos(v);
+	G.removeVertex(w);
+	cout<<"输入路由器"<<endl;
+	 cin>>v;
+	  r=G.getVertexPos(v);
+	 if(r==-1)
+	 {
+	 	cout<<"没有该路由"<<endl;
+	 }
+	 else
+	 {	 
+	 ShortestPath(G, r, dist, path);
+     }
+     cout<<"删除一条边"<<endl;
+		cin>>v;
+		cin>>u;
+		r=G.getVertexPos(v);
+		h=G.getVertexPos(u);
+		while(!G.removeEdge(r,h))
+		{
+			cout<<"删除一条边"<<endl;
+			 cin>>v;
+		cin>>u;
+		r=G.getVertexPos(v);
+		h=G.getVertexPos(u);
+		}
+		cout<<"输入路由器"<<endl;
+	 cin>>v;
+	  r=G.getVertexPos(v);
+	 if(r==-1)
+	 {
+	 	cout<<"没有该路由"<<endl;
+	 }
+	 else
+	 {	 
+	 ShortestPath(G, r, dist, path);
+     }	
+	 
+}Table[i].adj=NULL;
  };
+ int main()
+{
+Graphlnk G(30);
+cout<<"读取文件插入站点" <<endl;
+G.insertVertex();
+cout<<"总站点数为"<<G.numVertices<<endl;
+int i,j,weight;
+ifstream fin("C:\\2.txt", std::ios::in);
+char line[1024]={0};
+string x = "";
+string y="";
+int z;
+while(fin.getline(line, sizeof(line)))
+{
+stringstream word(line);
+word >> x;
+word >> y;
+word >> z;
+i=G.getVertexPos(x);
+j=G.getVertexPos(y);
+weight=z;
+G.insertEdge(i,j,weight);
+}
+
+fin.clear();
+fin.close();
+cout<<"总边数数为"<<G.numEdges<<endl;
+ 
+	//查询函数
+	int *dist= new int[G.numVertices];
+	 int *path= new int[G.numEdges];
+	 string v,u;
+	 cout<<"输入路由器"<<endl;
+	 cin>>v;
+	 int h;
+	 int r=G.getVertexPos(v);
+	 if(r==-1)
+	 {
+	 	cout<<"没有该路由"<<endl;
+	 }
+	 else
+	 {	 
+	 ShortestPath(G, r, dist, path);
+     }
+	cout<<"删除路由器"<<endl;
+	cin>>v;
+	int w=G.getVertexPos(v);
+	G.removeVertex(w);
+	cout<<"输入路由器"<<endl;
+	 cin>>v;
+	  r=G.getVertexPos(v);
+	 if(r==-1)
+	 {
+	 	cout<<"没有该路由"<<endl;
+	 }
+	 else
+	 {	 
+	 ShortestPath(G, r, dist, path);
+     }
+     cout<<"删除一条边"<<endl;
+		cin>>v;
+		cin>>u;
+		r=G.getVertexPos(v);
+		h=G.getVertexPos(u);
+		while(!G.removeEdge(r,h))
+		{
+			cout<<"删除一条边"<<endl;
+			 cin>>v;
+		cin>>u;
+		r=G.getVertexPos(v);
+		h=G.getVertexPos(u);
+		}
+		cout<<"输入路由器"<<endl;
+	 cin>>v;
+	  r=G.getVertexPos(v);
+	 if(r==-1)
+	 {
+	 	cout<<"没有该路由"<<endl;
+	 }
+	 else
+	 {	 
+	 ShortestPath(G, r, dist, path);
+     }	
+	 
+}
  
