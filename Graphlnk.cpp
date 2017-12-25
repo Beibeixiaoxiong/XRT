ca9e5c7d5a61d6f984f5c75905ea01af3aa9deb9
xiongruoting <34154858+xiongruoting@users.noreply.github.com>
2017-12-25 16:38:40 +0800

    Update Graphlnk.cpp

--- Graphlnk.cpp
+++ Graphlnk.cpp
@@ -1,7 +1,7 @@
 #include"Graph.h"
 int Graphlnk::getWeight(int v1,int v2)
 {
-	if( v1 != -1 && v2 != -1)//两个结点都存在
+	if( v1 != -1 && v2 != -1)
 	{
 		Edge *p = NodeTable[v1].adj;
 		while (p!= NULL && p->dest!= NodeTable[v2].start)
@@ -13,7 +13,6 @@ int Graphlnk::getWeight(int v1,int v2)
 		else return DefaultVertices+1;
 	}
 }
-
  int Graphlnk::getNextNeighbor(int v,int w)
  {
  if(v!=-1)
@@ -44,91 +43,8 @@ Graphlnk::Graphlnk(int sz)
  	numEdges=0;
  	NodeTable=new Vertex[sz];
  	for(int i=0;i<maxVertices;i++)
- 	Node int main()
-{
-Graphlnk G(30);
-cout<<"读取文件插入站点" <<endl;
-G.insertVertex();
-cout<<"总站点数为"<<G.numVertices<<endl;
-int i,j,weight;
-ifstream fin("C:\\2.txt", std::ios::in);
-char line[1024]={0};
-string x = "";
-string y="";
-int z;
-while(fin.getline(line, sizeof(line)))
-{
-stringstream word(line);
-word >> x;
-word >> y;
-word >> z;
-i=G.getVertexPos(x);
-j=G.getVertexPos(y);
-weight=z;
-G.insertEdge(i,j,weight);
-}
-
-fin.clear();
-fin.close();
-cout<<"总边数数为"<<G.numEdges<<endl;
- 
-	//查询函数
-	int *dist= new int[G.numVertices];
-	 int *path= new int[G.numEdges];
-	 string v,u;
-	 cout<<"输入路由器"<<endl;
-	 cin>>v;
-	 int h;
-	 int r=G.getVertexPos(v);
-	 if(r==-1)
-	 {
-	 	cout<<"没有该路由"<<endl;
-	 }
-	 else
-	 {	 
-	 ShortestPath(G, r, dist, path);
-     }
-	cout<<"删除路由器"<<endl;
-	cin>>v;
-	int w=G.getVertexPos(v);
-	G.removeVertex(w);
-	cout<<"输入路由器"<<endl;
-	 cin>>v;
-	  r=G.getVertexPos(v);
-	 if(r==-1)
-	 {
-	 	cout<<"没有该路由"<<endl;
-	 }
-	 else
-	 {	 
-	 ShortestPath(G, r, dist, path);
-     }
-     cout<<"删除一条边"<<endl;
-		cin>>v;
-		cin>>u;
-		r=G.getVertexPos(v);
-		h=G.getVertexPos(u);
-		while(!G.removeEdge(r,h))
-		{
-			cout<<"删除一条边"<<endl;
-			 cin>>v;
-		cin>>u;
-		r=G.getVertexPos(v);
-		h=G.getVertexPos(u);
-		}
-		cout<<"输入路由器"<<endl;
-	 cin>>v;
-	  r=G.getVertexPos(v);
-	 if(r==-1)
-	 {
-	 	cout<<"没有该路由"<<endl;
-	 }
-	 else
-	 {	 
-	 ShortestPath(G, r, dist, path);
-     }	
-	 
-}Table[i].adj=NULL;
+ 	NodeTable[i].adj=NULL;
+ 	
  };
  bool Graphlnk::removeEdge(int v1,int v2)//图上删一条边 
  {
@@ -156,14 +72,19 @@ cout<<"总边数数为"<<G.numEdges<<endl;
 	 if(p==s) NodeTable[v2].adj=p->link;
 	 else q->link=p->link;
 	 delete p;
+	 cout<<"删除成功"<<endl;
 	 return true;
-  } return false;
+  }
+  else
+  {
+ cout<<"结点错误，删边失败"<<endl;   
+  return false;
+}
 }
 bool Graphlnk::removeVertex(int v)
 {
 	if(v<0||v>=numVertices) 
-{
-	cout<<"该结点错误"<<endl;	
+{	
 	return false;
 }
 Edge*p,*s,*t;
@@ -190,3 +111,73 @@ while(NodeTable[v].adj!=NULL)
 	delete p;
 	numEdges--;
 }
+
+numVertices--;
+NodeTable[v].start=NodeTable[numVertices].start;
+p=NodeTable[v].adj=NodeTable[numVertices].adj;
+if(p!=NULL)
+{
+	
+	s=NodeTable[getVertexPos(p->dest)].adj;
+	while(s!=NULL)
+	
+	if(getVertexPos(s->dest)==numVertices)
+	{
+		
+		s->dest=NodeTable[v].start;break;
+	}
+	else s=s->link;
+
+}
+cout<<"删除路由器成功"<<endl;
+return true;
+}
+bool Graphlnk::insertVertex()//插入一个结点 
+	{	 
+ifstream fin("C:\\1.txt", std::ios::in);
+char line[1024]={0};
+ numVertices=0;
+string x = "";
+while(fin.getline(line, sizeof(line)))
+{
+stringstream word(line);
+word >> x;
+NodeTable[numVertices].start=x;
+ numVertices++;
+}
+
+fin.clear();
+fin.close();
+cout<<"插入结点成功"<<endl;
+return true;
+	 };	 
+bool Graphlnk::insertEdge(int v1,int v2,int cost,string v3,string v4)
+{
+
+	if(v1>=0&&v1<=numVertices&&v2>=0&&v2<=numVertices)//v1,v2合法 
+		{
+			string a;
+			int b,c;
+		 Edge*q,*p;
+		 p=NodeTable[v1].adj;
+		 while(p!=NULL&&p->dest!=NodeTable[v2].start)
+		 p=p->link;
+		 if(p!=NULL) 
+		 {
+		 return false;
+		 } //找到此边不插入
+		 p=new Edge(); q=new Edge();
+		 p->dest=NodeTable[v2].start;    p->cost=cost; 
+		 p->netnumber=v3;p->yanma=v4;
+		 p->link=NodeTable[v1].adj;
+		 NodeTable[v1].adj=p;//链入v1表 
+		 q->dest=NodeTable[v1].start;    q->cost=cost;
+		 q->netnumber=v3;q->yanma=v4;
+		 q->link=NodeTable[v2].adj;
+		 NodeTable[v2].adj=q; 
+		 numEdges++;
+		 return true;
+		}
+		return false;
+
+	 } ;
